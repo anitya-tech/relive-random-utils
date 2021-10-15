299683ad8bc3d708106dcca4796547bf84d44b94
GeekTR <geektheripper@gmail.com>
2021-10-16 03:31:43 +0800

    copy local backup to s3

---
+++ src/backup-gifts/local-to-s3.ts
@@ -0,0 +1,59 @@
+import fs from "fs/promises";
+import path from "path";
+
+import { ReceivedGiftStreamList } from "@gtr/random-bilibili-api/dist/apis/live/received-gift-stream-list";
+import moment from "moment-timezone";
+
+import { getS3, S3Bucket, S3KeyPrefix } from "./config";
+
+const datastore = "/home/geektr/playground/bilibili-gifts/datastore";
+const stopDate = moment("2021-04-18");
+
+async function start() {
+  const s3 = await getS3();
+  console.log("load: s3");
+
+  for (const uid of await fs.readdir(datastore)) {
+    for (const file of await fs.readdir(path.join(datastore, uid))) {
+      const [, _date] = file.match(/(\d{4}-\d{2}-\d{2})/) as string[];
+      const date = moment(_date);
+      if (!date.isBefore(stopDate)) continue;
+      console.log(uid, _date, date, date.format("YYYY-MM-DD"));
+
+      const KeyPreifx = `${S3KeyPrefix}/${uid}/${date.format(
+        "YYYY"
+      )}/${date.format("MM-DD")}`;
+      const JsonMetadata = {
+        "content-type": "application/json",
+        "cache-control": "public, max-age=31536000",
+      };
+
+      const gifts: ReceivedGiftStreamList.Record[] = JSON.parse(
+        await fs.readFile(path.join(datastore, uid, file), "utf-8")
+      );
+      if (gifts.length) {
+        await s3
+          .putObject({
+            Bucket: S3Bucket,
+            Key: `${KeyPreifx}.all.json`,
+            Metadata: JsonMetadata,
+            Body: JSON.stringify(gifts),
+          })
+          .promise();
+      }
+      const goldGifts = gifts.filter((i) => i.gold);
+      if (goldGifts.length) {
+        await s3
+          .putObject({
+            Bucket: S3Bucket,
+            Key: `${KeyPreifx}.gold.json`,
+            Metadata: JsonMetadata,
+            Body: JSON.stringify(goldGifts),
+          })
+          .promise();
+      }
+    }
+  }
+}
+
+start();
